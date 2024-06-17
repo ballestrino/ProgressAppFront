12acf94335e30451269cd8c7f3c52e6ed5dc4343
Nacho <nachoballestrino02@gmail.com>
2024-06-17 17:41:56 -0300

    Home design and basic components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import Inicio from './views/Inicio'
+import Home from './views/Home'
 import Goals from './views/Goals'
 import Projects from './views/Projects'
 
@@ -8,7 +8,7 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Inicio />} />
+        <Route path='/' element={<Home />} />
         <Route path='/projects' element={<Projects />} />
         <Route path='/goals' element={<Goals />} />
       </Routes>
