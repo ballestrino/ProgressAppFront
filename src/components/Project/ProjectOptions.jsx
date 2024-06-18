28b799aabd9424aca64e031c7eeb0b2398b2f29d
Nacho <nachoballestrino02@gmail.com>
2024-06-18 19:04:51 -0300

    Dropdown dissapear on click outside

--- src/components/Project/ProjectOptions.jsx
+++ src/components/Project/ProjectOptions.jsx
@@ -1,18 +1,40 @@
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import DotsSvg from '../../assets/threeDots.svg'
 import OptionsDropdown from './OptionsDropdown.jsx'
 
 export default function ProjectOptions() {
   const [showOptions, setShowOptions] = useState(false)
+
+  useEffect(() => {
+    const handleClickOutside = e => {
+      if (!refBox.current.contains(e.target) && showOptions) {
+        setShowOptions(false)
+      } else {
+        return
+      }
+    }
+    document.addEventListener('click', handleClickOutside, true)
+    return () => {
+      document.removeEventListener('click', handleClickOutside, true)
+    }
+  }, [showOptions])
+
+  const refBox = useRef(null)
+
   return (
-    <div className='flex justify-center items-center relative'>
+    <div className='flex justify-center items-center relative' ref={refBox}>
       <button
         className=''
         onClick={() => setShowOptions(prev => (prev ? false : true))}
       >
         <img src={DotsSvg} alt='Options' className='h-5' />
       </button>
-      {showOptions ? <OptionsDropdown /> : null}
+      {showOptions ? (
+        <OptionsDropdown
+          showOptions={showOptions}
+          setShowOptions={setShowOptions}
+        />
+      ) : null}
     </div>
   )
 }
