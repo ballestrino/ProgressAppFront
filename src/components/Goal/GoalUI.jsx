fbaecc9dee201105192ec613c6e42a7b469626d5
Nacho <nachoballestrino02@gmail.com>
2024-06-20 18:59:16 -0300

    adding a go back button in goals

--- src/components/Goal/GoalUI.jsx
+++ src/components/Goal/GoalUI.jsx
@@ -1,8 +1,10 @@
-import { useLocation } from 'react-router'
+import { useLocation, useNavigate } from 'react-router'
 import Goal from './Goal'
+import LeftShortArrow from '../../assets/icons/arrow-left-short.svg'
 
 export default function GoalUI() {
   const location = useLocation()
+  const navigate = useNavigate()
   const queryParams = new URLSearchParams(location.search)
   const id = queryParams.get('id') // id para obtener las metas de un proyecto
   const title = queryParams.get('title')
@@ -25,10 +27,19 @@ export default function GoalUI() {
     }
   ]
 
+  const handleReturn = () => {
+    navigate('/projects')
+  }
+
   return (
     <div className='flex w-screen h-screen justify-center items-center'>
       <div className='flex flex-col w-2/5 h-screen py-20 gap-5'>
-        <h1 className='text-4xl'>{title}</h1>
+        <section className='flex w-full items-center gap-1 px-4'>
+          <button alt='go back' onClick={handleReturn}>
+            <img src={LeftShortArrow} alt='left arrow' className='w-5 h-5' />
+          </button>
+          <h1 className='text-4xl'>{title}</h1>
+        </section>
         <section className='grid grid-cols-1 gap-y-5'>
           {goals.map(goal => (
             <Goal key={goal.id} goal={goal} />
